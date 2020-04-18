f2f9a0a0925f16db3a54e7feadb7c488fb6d2ae0
Judson Douglas <judson.douglas@metova.com>
2020-04-18 13:41:40 -0500

    test change to app.js from dev container

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Shut up Pig Cow Farm Animalç
+          Shut up Pig Cow Farm Animal Cult Jedi
         </p>
         <a
           className="App-link"
